10b0c5433b5932f6237e6f0e4f951ae803acba49
Fergus Molloy <fdm504@york.ac.uk>
2019-10-10 17:26:23 +0100

    Tidied some code

--- LEDS.c
+++ LEDS.c
@@ -2,28 +2,35 @@
 #include "LEDS.h"
 #include "lpc17xx_systick.h"
 //#include "serial/serial.h"
-int count=0;
-int flag=0;
 
 int main(void){
-	GPIO_SetDir(1, ALLLEDS, 1);
-	SYSTICK_InternalInit(100);
-	SYSTICK_IntCmd(ENABLE);
-	SYSTICK_Cmd(ENABLE);
+	setup()
 	return 0;
 }
 
+void setup(){
+	SYSTICK_InternalInit(100);
+	SYSTICK_IntCmd(ENABLE);
+	SYSTICK_Cmd(ENABLE);
+}
+
+void SetLED(uint32_t bitmap){
+	GPIO_SetDir(1, bitmap, 1);
+	GPIO_SetValue(1, bitmap);
+}
+
 void SysTick_Handler(){
-	//write_usb_serial_blocking("called", 7);
+	static int count=0;
+	static int flag=0;
 	if(count==10){
 		count = 0;
 		if(flag){
 			flag = 0;
-			GPIO_SetValue(1, ALLLEDS);
+			SetLED(ALLLEDS);
 		}
 		else{
 			flag = 1;
-			GPIO_SetValue(1, 0);
+			SetLED(0); // 0 turns all outputs off
 		}
 	}
 }
--- LEDS.h
+++ LEDS.h
@@ -4,3 +4,7 @@
 #define LED3 0x00800000
 
 const uint32_t ALLLEDS = LED0|LED1|LED2|LED3;
+
+void setup();
+
+void SetLEDS(uint32_t bitmap);
