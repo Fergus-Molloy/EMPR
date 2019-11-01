73a9cc49b7d5bc2df53605546fa912bf5481b6bc
Fergus Molloy <fdm504@york.ac.uk>
2019-11-01 12:37:43 +0000

    got writing to LCD working

--- I2C.c
+++ I2C.c
@@ -11,21 +11,52 @@ int size(char string[]){
 }
 
 void LCD_setup(){
-  char buf[] = {0x00,0x34,0x0c,0x06,0x35,0x04,0x10,0x42,0x9f,0x34,0x02};
-  transmit(buf, SERIAL_ADDR);
+  char buf[11] = {0x00,0x34,0x0c,0x06,0x35,0x04,0x10,0x42,0x9f,0x34,0x02};
+  transmit(buf, SERIAL_ADDR, 11);
+}
+
+void LCD_write(char address, char letter){
+  char addr[2] = {0x00, address};
+  char buf[2] = {0x40, letter};
+  transmit(addr, SERIAL_ADDR, 2);
+  transmit(buf, SERIAL_ADDR, 2);
+}
+
+void LCD_clear(){
+  char x;
+  for(x=0x80; x<0xB8; x++){ //B8 is the last address we can see
+    LCD_write(x, 0xA0);
+  }
 }
 
 int main(void){
   serial_init();
   setup_I2C(3,0,0,1);
-  int result = 0;
   LCD_setup();
-  char buf[2] = {0x00, 0x80};
-  transmit(buf, SERIAL_ADDR);
-  buf[0] = 0x40;
-  buf[1] = 0x64;
-  result = transmit(buf, SERIAL_ADDR);
+  LCD_clear();
+
+  char buf[16] = {0};
+  buf[0] = 200;
+  buf[1] = 101;
+  buf[2] = 108;
+  buf[3] = 108;
+  buf[4] = 111;
+  buf[5] = SPACE;
+  buf[6] = 87;
+  buf[7] = 111;
+  buf[8] = 114;
+  buf[9] = 108;
+  buf[10] = 100;
+
+  char addr=0;
+  addr = 0x80;
+  int x;
+  for(x=0; x<11; x++){
+    LCD_write(addr, buf[x]);
+    addr++;
+  }
   write_usb_serial_blocking("Success\n\r", 9);
+
   return 0;
 }
 
@@ -46,11 +77,11 @@ void setup_I2C(int func, int port, int pin0, int pin1){
   I2C_Cmd(LPC_I2C1, ENABLE);
 }
 
-int transmit(char data[], uint8_t addr){
+int transmit(char data[], uint8_t addr, int size){
   I2C_M_SETUP_Type transferMCfg;
   transferMCfg.sl_addr7bit = addr;
   transferMCfg.tx_data = data;
-  transferMCfg.tx_length = sizeof(data);
+  transferMCfg.tx_length = size;
   transferMCfg.rx_data = NULL;
   transferMCfg.rx_length = 0;
   transferMCfg.retransmissions_max = 3;
---
+++ ascii.py
@@ -0,0 +1,9 @@
+
+
+def convert(yes):
+    arr=[]
+    for x in yes:
+        arr.append(ord(x))
+    return arr
+
+print(convert("Hello World"))
